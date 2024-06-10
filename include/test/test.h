fe2600c58cdb344c32a96c0901c80171f4ab4b41
fgwsz <3191000458@qq.com>
2024-06-10 20:03:34 +0800

    clean

--- include/test/test.h
+++ include/test/test.h
@@ -18,29 +18,29 @@ void case_pass_count_increment(void)noexcept;
 }//namespace test::detail
 }//namespace test
 //public
-#define TEST_UNIT(unit_name__) \
+#define TEST_UNIT(unit_name__)                             \
     static void test_unit_function_of_##unit_name__(void); \
-    static bool test_unit_register_of_##unit_name__= \
-    ::test::detail::unit_add( \
-        #unit_name__, \
-        ::std::function<void(void)>{ \
-            test_unit_function_of_##unit_name__ \
-        } \
-    ); \
-    void test_unit_function_of_##unit_name__(void) \
+    static bool test_unit_register_of_##unit_name__=       \
+        ::test::detail::unit_add(                          \
+            #unit_name__,                                  \
+            ::std::function<void(void)>{                   \
+                test_unit_function_of_##unit_name__        \
+            }                                              \
+        );                                                 \
+    void test_unit_function_of_##unit_name__(void)         \
 //
 //public
-#define TEST_CASE(...) do{ \
-    ::test::detail::case_count_incement(); \
-    if(!(__VA_ARGS__)){ \
-        ::test::detail::case_fail_count_increment(); \
-        ::test::detail::throw_error(::std::runtime_error( \
-            "\t\t<file> "+::std::string(__FILE__)+"\n" \
-            "\t\t<line> "+::std::to_string(__LINE__)+"\n" \
-            "\t\t<case> "+::std::string(#__VA_ARGS__) \
-        )); \
-    }else{ \
-        ::test::detail::case_pass_count_increment(); \
-    } \
-}while(0) \
+#define TEST_CASE(...) do{                                 \
+    ::test::detail::case_count_incement();                 \
+    if(!(__VA_ARGS__)){                                    \
+        ::test::detail::case_fail_count_increment();       \
+        ::test::detail::throw_error(::std::runtime_error(  \
+            "\t\t<file> "+::std::string(__FILE__)+"\n"     \
+            "\t\t<line> "+::std::to_string(__LINE__)+"\n"  \
+            "\t\t<case> "+::std::string(#__VA_ARGS__)      \
+        ));                                                \
+    }else{                                                 \
+        ::test::detail::case_pass_count_increment();       \
+    }                                                      \
+}while(0)                                                  \
 //
