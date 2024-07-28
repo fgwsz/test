a08dd97cb8e026286daf919858437da6f88ce6d4
fgwsz <3191000458@qq.com>
2024-07-28 12:34:21 +0800

    clean

--- git-push.ps1
+++ git-push.ps1
@@ -1,6 +1,8 @@
+$root_path=Split-Path -Parent $MyInvocation.MyCommand.Definition
 echo "You Can Input q For Abort."
 $commit_info=Read-Host -Prompt "Input Git Commit Info"
 if(!($commit_info -eq "q")){
+    cd $root_path
     git add ./include/*
     git add ./src/*
     git add ./tests/*
